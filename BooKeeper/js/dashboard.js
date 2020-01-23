1e7822d47a9f24889e07e33be87271d2fc467307
gdara17 <gdara17@freeuni.edu.ge>
2020-01-24 01:21:52 +0400

    filtering and sidebar

--- BooKeeper/js/dashboard.js
+++ BooKeeper/js/dashboard.js
@@ -76,9 +76,27 @@ class Term {
 
 
 handleDashboard = () => {
+    const mainContainerHTML = `
+        <div class="center-container"></div>
+        <div class="details-panel">
+            <div class="details-container">
+                <div class="details-exit">
+                    <a href="#">X</a>
+                </div>
+                <h2>Course Name</h2>
+                <h3>Tutor</h3>
+                <div class="details-course-description"></div>
+                <div class="details-scores"></div>
+                <div class="details-chart">
+                    <img src="../BooKeeper/assets/Grade Distribution.png" alt="Grade Distribution">
+                </div>
+            </div>
+        </div>
+    `;
+    document.querySelector(".main-container").innerHTML = mainContainerHTML;
     container = document.querySelector(".center-container");
     container.innerHTML = "";
-    terms = fetch("/BooKeeper/data/courses.json")
+    terms = fetch("../BooKeeper/data/courses.json")
         .then(response => response.json())
         .then(terms => {
             for (term of terms) {
@@ -89,7 +107,71 @@ handleDashboard = () => {
         });
 }
 
-handleDashboard();
+handleCourses = () => {
+    mainContainer = document.querySelector(".main-container");
+    filterContainer = `
+        <div class="filter-container">
+            <div class="filter">
+                <h2>Filter</h2>
+                <div class="field">
+                    <label for="category">Category</label>
+                    <select name="category">
+                        <option value="MACS">MACS</option>
+                        <option value="ECON">ECON</option>
+                        <option value="PHY">PHY</option>
+                    </select>
+                </div>
+                <div class="field">
+                    <label for="credits">Credits</label>
+                    <select name="credits">
+                        <option value="MACS">MACS</option>
+                        <option value="ECON">ECON</option>
+                        <option value="PHY">PHY</option>
+                    </select>
+                </div>
+                <div class="field">
+                    <label for="category">Tutor</label>
+                    <select name="tutor">
+                        <option value="MACS">MACS</option>
+                        <option value="ECON">ECON</option>
+                        <option value="PHY">PHY</option>
+                    </select>
+                </div>
+                <div class="field">
+                    <label for="selected">Selected</label>
+                    <select name="selected">
+                        <option value="Selected">Selected</option>
+                        <option value="Not Selected">Not Selected</option>
+                    </select>
+                </div>
+                <button>
+                    APPLY
+                </button>
+            </div>
+        </div>
+    `;
+    mainContainer.innerHTML = filterContainer + mainContainer.innerHTML;
+}
+
+
+var useHash = true;
+var hash = '#';
+var router = new Navigo(null, useHash, hash);
+
+router.on({
+	"/dashboard": () => {
+        handleDashboard();
+		console.log("dashboard");
+	},
+	"/courses": () => {
+        handleCourses();
+		console.log("courses");
+	},
+	"*": () => {
+		console.log("home");
+	}
+}).resolve();
+
 
 document.addEventListener("click", (e) => {
     if (e.target.closest(".course-entry-container")) {
@@ -99,7 +181,7 @@ document.addEventListener("click", (e) => {
         const courseDetails = document.querySelector(".details-panel");
 
         // Terrible way of fetching course by ID
-        terms = fetch("/BooKeeper/data/courses.json")
+        terms = fetch("../BooKeeper/data/courses.json")
             .then(response => response.json())
             .then(terms => {
                 let courseById;
