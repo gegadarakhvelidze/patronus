ec4ef64347a549c63129c9c81fe6ae3a1fa9cfe5
gdara17 <gdara17@freeuni.edu.ge>
2020-11-14 23:23:17 +0400

    responsive dashboard

--- BooKeeper/js/dashboard.js
+++ BooKeeper/js/dashboard.js
@@ -2,9 +2,20 @@ document.querySelectorAll(".course-entry-container").forEach(subject => {
     subject.addEventListener("click", () => {
         const container = document.querySelector(".main-container")
         const centerContainer = document.querySelector(".center-container")
-        const courseDetails = document.querySelector(".course-details-panel")
-        container.style.width = "50%"
-        centerContainer.style.width = "80%"
+        const courseDetails = document.querySelector(".details-panel")
+
+        container.classList.add("main-container-shrinked")
+        centerContainer.classList.add("center-container-shrinked")
         courseDetails.style.display = "block"
     })
+})
+
+document.querySelector(".details-exit").addEventListener("click", () => {
+    const container = document.querySelector(".main-container")
+    const centerContainer = document.querySelector(".center-container")
+    const courseDetails = document.querySelector(".details-panel")
+    
+    container.classList.remove("main-container-shrinked")
+    centerContainer.classList.remove("center-container-shrinked")
+    courseDetails.style.display = "none"
 })
